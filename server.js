658bf6c3f3a1c59a10643557c15da2c9a641c601
HeyMichelle <miscmiche@gmail.com>
2020-08-05 21:27:30 -0400

    Completed responses for delete, get, get by id, and post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const express = require('express');
+const server = express();
 
-// Step 1: import dependency from node_module (express) using require()
-const express = require("express")
 
 // Step 2: import local file for database
 const db = require("./database")
@@ -9,72 +9,144 @@ const db = require("./database")
 const shortid = require("shortid")
 
 // Step 4: create a new express server and add middleware to parse json reqs
-const server = express()
 server.use(express.json())
 
 // Step 5: response for if someone hits this API, equiv. to home page
 server.get("/", (req, res) => {
-	res.json({ message: "Intro to Node.js" })
+	res.json({ message: "Intro to Node.js and Express Server" })
+	// res.send('Welcome to API Project One')
 })
 
 // Step 6: Getting response for list of users from "fake" databse
-server.get("/users", (req, res) => {
+server.get("/api/users", (req, res) => {
 	// gets a list of users from he "fake" database
-	const users = db.getUsers()
-    res.json(users)
-    res.status(200).json(users)
+    const users = db.getUsers()
+    // can also do the above with: const users = req.body;
+
+    if (users) {
+        res.status(201).json(users) // correctly working, send users as json response
+    } else {
+        res.status(500).json({ errorMessage: "The user's information could not be retrieved." })
+    }  
+
 })
 
 // Step 7: Getting process and error response for getting a user by id
-server.get("/users/:id", (req, res) => {
+server.get("/api/users/:id", (req, res) => {
 	// the param variable matches up to the name of our URL param above
-	const id = req.params.id
+    const { id } = req.params;
+    
 	// get a specific user by their ID from the "fake" database
-	const user = db.getUserById(id)
+    const users = db.getUsers();    
+    // can also be written: const user = req.body;
+    const foundById = users.find(user => user.id === id);
+
+
+    // shorten and combine: can also be written as: const users = db.getUserById(id);
+
+    if (!foundById) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    }
+
+    if (!users) {
+        res.status(500).json({ errorMessage: "The user's information could not be retrieved." })
+    }
 
 	// make sure the system doesn't break if someone calls the endpoint with
 	// a user ID that doesn't exist in the database
 	if (user) {
-		res.json(user)
-	} else {
-		res.status(404).json({ message: "User not found" })
-	}
+        res.status(201)
+	} 
+    
 })
 
 // Step 8: npm i shortid, so that you can use shortid.generate() to make id for user post
 // Step 9: Posting process and error response for posting to user api
-server.post("/users", (req, res) => {
+server.post("/api/users", (req, res) => {
 	// creates a new user in our "fake" database and returns the new user data
-	const newUser = db.createUser({
-		id: shortid.generate(),
-		name: "Jane Doe", // String, required
-		bio: "Not Tarzan's Wife, another Jane",  // String, required
-	})
-	res.status(201).json(newUser)
+	const newUser = db.createUser({ id: shortid.generate(), ...req.body })
+        // req.body meaning body of the object/data in the object. Could be name and bio both. to access specifically you would do req.body.name or req.body.bio
+		// bio: req.body.bio,  // String, required
+    
+    if (!newUser.name || newUser.bio) {
+        res.status(400).json{{ errorMessage: "Please provide a name and a bio for the new user" }}
+    }
+
+    if (newUser) {
+        res.status(201).json(newUser)
+    } else {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+    
 })
 
 // Step 10: Deleting process and error response for deleting user by ID
-server.delete("/users/:id", (req, res) => {
-	const user = db.getUserById(req.params.id)
-
+server.delete("/api/users/:id", (req, res) => {
+	const userById = db.getUserById(req.params.id)
+    
 	// Step 10a: make sure the user exists before we try to delete it
-	if (user) {
+	if (userById) {
 		db.deleteUser(req.params.id)
 		res.status(204).end()
 		// use .end() when nothing is returned to make it finish using request and not keep waiting
 		// since we have nothing to return back to the client, send a 204 with an empty response.
 		// 204 just means "success but we have nothing to return".
-	} else {
-		res.status(404).json({
-			message: "User not found",
+    } else {
+		res.status(500).json({
+			errorMessage: "The user could not be removed"
 		})
-	}
+    }
+    
+    if (!userById) {
+        res.status(404).json({
+			message: "The user with the specified ID does not exist."
+        })
+    }
 })
 
+// server.delete('/api/users/:id', (req, res) => {
+//     const { id } = req.params;
+//     const found = users.find(user => user.id === id);
+  
+//     if (found) {
+//       users = users.filter(user => user.id !== id)
+//       res.status(200).json(users)
+//     } else {
+//       res.status(500).json({ errorMessage: "The user could not be removed" })
+//     }
+  
+//     if (!found) {
+//       res.status(404).json({ message: "The user with the specified ID does not exist" })
+//     }
+//   })
+
 //Step 11: Puting process and error response for editing method by user id
+server.put("/api/users/:id", (req, res) => {
+	const { id, name, bio } = req.params;
+	const changes = req.body;
+	
+	let index = users.findIndex(user => user.id === id)
+
+		if(index !== -1) {
+			changes.id = id;
+			users[index] = changes;
+			res.status(200).json(users[index]);
+		}
+
+		if (!index) {
+			res.status(404).json({ message: "The user with the specified ID does not exist." })
+		}
+
+		if (!name || !bio) {
+			res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+		}
+
+		if (!changes) {
+			res.status(500).json({ errorMessage: "The user information could not be modified." })
+		}
+})
 
 
-// Step 3: to start, use node server.js in terminal
-server.listen(8080, () => {
-    console.log("Server started on port 8080")
-}) 
+
+
+module.exports = server;
